55787fa064a8c9be4df944deabb15ae9f002f897
Leon Spaans <lspaans@gmail.com>
2013-12-28 18:27:04 -0600

    Backlog commit 1/5.

--- sudoku.py
+++ sudoku.py
@@ -2,35 +2,47 @@
 
 import os
 
-DEF_GRID_BASE = 3
+DEF_TILE_BASE = 3
 
 class Cell(object):
-    def __init__(self, value=None, gridBase=DEF_GRID_BASE, options=[]):
-        self.__gridBase = gridBase
-        self.maxValue = self.__gridBase ** 2
+    def __init__(self, value=None, tileBase=DEF_TILE_BASE, options=[]):
+        self._tileBase = tileBase
+        self.maxValue = self._tileBase ** 2
         self.allOptions = range(1, self.maxValue + 1)
         self.options = self.allOptions
         self.value = value
 
     @property
     def maxValue(self):
-        return self.__maxValue
+        return self._maxValue
 
     @property
     def options(self):
-        return tuple(self.__options)
+        return tuple(self._options)
 
     @property
     def value(self):
-        return self.__value
+        return self._value
+
+    @property
+    def col(self):
+        return self._col
+
+    @property
+    def row(self):
+        return self._row
+
+    @property
+    def tile(self):
+        return self._tile
 
     @maxValue.setter
     def maxValue(self, maxValue):
-        self.__maxValue = maxValue
+        self._maxValue = maxValue
 
     @options.setter
     def options(self, options=[]):
-        self.__options = set(options)
+        self._options = set(options)
 
     def delOptions(self, options=[]):
        self.options = tuple(set(self.options) - set(options))
@@ -38,45 +50,76 @@ class Cell(object):
     @value.setter
     def value(self, value=None):
         if value in self.options:
-            self.__value, self.options = value, [value]
+            self._value, self.options = value, [value]
         elif value is None:
-            self.__value = None 
+            self._value = None 
             self.options = self.allOptions
         else:
             raise ValueError("Invalid value")
 
+    @col.setter
+    def col(self, col):
+        self._col = col
+
+    @row.setter
+    def row(self, row):
+        self._row = row
+
+    @tile.setter
+    def tile(self, tile):
+        self._tile = tile
+
     def __str__(self):
-        return str(self.__value)
+        return str(self._value)
 
     def __repr__(self):
         return self.__str__()
 
-class CellList(object):
-    def __init__(self, values=[], gridBase=DEF_GRID_BASE):
-        self.__gridBase = gridBase
-        self.__maxCells = self.__gridBase ** 2
-        self.cells = values
+class CellGroup(object):
+    def __init__(self, maxCells=DEF_TILE_BASE ** 2, tileBase=DEF_TILE_BASE):
+        self._maxCells = maxCells
+        self._tileBase = tileBase
+        self._cells = []
 
     @property
     def cells(self):
-        return self.__cells
+        return self._cells
 
     @cells.setter
-    def cells(self, values=None):
-        if values is None or len(values) == 0:
-            values = range(1, self.__maxCells + 1)
+    def cells(self, cells=[]):
+        for cell in cells:
+            self.addCell(cell)
 
-        if len(set(values)) != self.__maxCells and set(values) != set([None]):
-            raise ValueError("Non-unique or invalid number of values")
+# HIERO CONTROLEREN OF VALUE VAN CELL UNIQUE IS
 
-        self.__cells = [Cell(v) for v in values]
+    def addCell(self, cell):
+        if self._maxCells <= len(self._cells):
+            print(repr(self._cells))
+            raise ValueError("Too many cells")
+        self._cells.append(cell)
 
-class Tile(CellList):
+class Col(CellGroup):
+    def __str__(self):
+        out = "\n".join(map(lambda c: str(c), self.cells))
+        return out
+
+    def __repr__(self):
+        return self.__str__()
+
+class Row(CellGroup):
+    def __str__(self):
+        out = " ".join(map(lambda c: str(c), self.cells))
+        return out
+
+    def __repr__(self):
+        return self.__str__()
+
+class Tile(CellGroup):
     def __str__(self):
         out = ""
-        for y in xrange(self.__gridBase):
-            s = y * self.__gridBase
-            e = s + self.__gridBase
+        for y in xrange(self._tileBase):
+            s = y * self._tileBase
+            e = s + self._tileBase
             out += "{0}\n".format(
                     " ".join(map(lambda c: str(c), self.cells[s:e]))
             )
@@ -85,44 +128,97 @@ class Tile(CellList):
     def __repr__(self):
         return self.__str__()
 
-class Row(CellList):
-    pass
-
-class Row(CellList):
-    pass
-
 class Board(object):
-    def __init__(self, gridBase=DEF_GRID_BASE):
-        self.__gridBase = gridBase
-        self.__maxTiles = self.__gridBase ** 2
-        self.__tiles = [Tile() for n in xrange(self.__maxTiles)]
+    def __init__(self, tileBase=DEF_TILE_BASE):
+        self._tileBase = tileBase
+        self._cells = CellGroup(self._tileBase ** 4)
+        self._cols = []
+        self._rows = []
+        self._tiles = []
+        self.initCells()
+        self.initCellGroups()
+
+    def initCells(self):
+        for n in xrange(self._tileBase ** 4):
+            c = Cell(None)
+            c.row, c.col = divmod(n, self._tileBase ** 2)
+            c.tile = (
+                self._tileBase * (n // self._tileBase ** self._tileBase) +
+                (n % self._tileBase ** 2) // self._tileBase
+            )
+            self._cells.addCell(c)
+
+    def initCellGroups(self):
+        for n in xrange(self._tileBase ** 2):
+            col = Col()
+            row = Row()
+            tile = Tile()
+            for cell in self._cells.cells:
+                if cell.col == n:
+                    col.addCell(cell)
+                if cell.row == n:
+                    row.addCell(cell)
+                if cell.tile == n:
+                    tile.addCell(cell)
+            self._cols.append(col)
+            self._rows.append(row)
+            self._tiles.append(tile)
+
+    @property
+    def cell(self, cell):
+        if cell >= len(self._cells):
+            raise ValueError("Invalid cell")
+        return self._cells[cell]
+
+    @property
+    def cells(self):
+        return self._cells
+
+    def col(self, col):
+        if col >= self._tileBase ** 2:
+            raise ValueError("Invalid col")
+        return self._cols[col]
+
+    @property
+    def cols(self):
+        return self._cols
+
+    def row(self, row):
+        if row >= self._tileBase ** 2:
+            raise ValueError("Invalid row")
+        return self._rows[row]
+
+    @property
+    def rows(self):
+        return self._rows
+
+    def tile(self, tile):
+        if tile >= self._tileBase ** 2:
+            raise ValueError("Invalid tile")
+        return self._tiles[tile]
 
     @property
     def tiles(self):
-        return self.__tiles
+        return self._tiles
 
-    @property
-    def col(self, col):
-        pass
+    def __str__(self):
+        out = "\n".join(map(lambda r: str(r), self._rows))
+        return(out)
 
-    @property
-    def row(self, row):
-        pass
-
-    @col.setter
-    def col(self, col):
-        pass
-
-    @row.setter
-    def row(self, row):
-        pass
+    def __repr__(self):
+        return self.__str__()
 
 class Game(object):
-    def __init__(self, gridBase=DEF_GRID_BASE):
-        self.__board = Board()
+    def __init__(self, tileBase=DEF_TILE_BASE):
+        self._board = Board()
 
 if __name__ == '__main__':
     os.system('clear')
     b = Board()
-    print repr(b.tiles[0])
-
+    print(str(b))
+    print
+    print(str(b.cols[0]))
+    print
+    print(str(b.rows[0]))
+    print
+    print(str(b.cells.cells[0]))
