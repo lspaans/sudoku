910483c0e84e94e79b82519be695142db6514040
Léon Spaans <leons@gridpoint.nl>
2017-07-19 15:58:44 +0200

    Make better use of pytest fixtures

--- tests/test_setdict.py
+++ tests/test_setdict.py
@@ -8,7 +8,7 @@ from setdict import Setdict
 @pytest.fixture
 def setdict_object(scope="module"):
     """docstring"""
-    return Setdict()
+    return Setdict({tuple((1, 2, 3,)): "a"})
 
 
 def test_setdict___init___method(setdict_object):
@@ -18,15 +18,13 @@ def test_setdict___init___method(setdict_object):
 
 def test_setdict_value_setter(setdict_object):
     """docstring"""
-    setdict_object.value = {tuple((1, 2, 3,)): "a"}
+    setdict_object.value = {tuple((3, 2, 1)): "a"}
 
     assert len(setdict_object) == 1
 
 
 def test_setdict_value_setter_with_wrong_type(setdict_object):
     """docstring"""
-    setdict_object.value = {tuple((1, 2, 3,)): "a"}
-
     with pytest.raises(TypeError) as exception_info:
         setdict_object.value = {int(1): "a"}
 
@@ -35,8 +33,6 @@ def test_setdict_value_setter_with_wrong_type(setdict_object):
 
 def test_setdict_value_getter(setdict_object):
     """docstring"""
-    setdict_object.value = {tuple((1, 2, 3,)): "a"}
-
     assert len(setdict_object.value) == 1
 
 
@@ -49,8 +45,6 @@ def test_setdict_set(setdict_object):
 
 def test_setdict_set_with_wrong_type(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     with pytest.raises(TypeError) as exception_info:
         setdict_object.set(str("1"), "a")
 
@@ -59,15 +53,11 @@ def test_setdict_set_with_wrong_type(setdict_object):
 
 def test_setdict___contains__(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     assert set((1, 2, 3,)) in setdict_object
 
 
 def test_setdict___contains__with_wrong_type(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     with pytest.raises(TypeError) as exception_info:
         float(1) in setdict_object
 
@@ -76,36 +66,26 @@ def test_setdict___contains__with_wrong_type(setdict_object):
 
 def test_setdict_has_value(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     assert setdict_object.has_value("a")
 
 
 def test_setdict_get(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     assert setdict_object.get(set((1, 2, 3,))) == "a"
 
 
 def test_setdict_get_with_unused_default(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     assert setdict_object.get(set((1, 2, 3,)), "default") == "a"
 
 
 def test_setdict_get_with_used_default(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     assert setdict_object.get(set((2, 3, 4)), "default") == "default"
 
 
 def test_setdict_get_with_wrong_type(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     with pytest.raises(TypeError) as exception_info:
         setdict_object.get(int(1))
 
@@ -114,22 +94,16 @@ def test_setdict_get_with_wrong_type(setdict_object):
 
 def test_setdict_get_key(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     assert setdict_object.get_key("a") == set((1, 2, 3,))
 
 
 def test_setdict_get_value(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     assert setdict_object.get_value(set((1, 2, 3,))) == "a"
 
 
 def test_setdict_get_value_with_wrong_type(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     with pytest.raises(TypeError) as exception_info:
         setdict_object.get_value(str("1"))
 
@@ -138,13 +112,9 @@ def test_setdict_get_value_with_wrong_type(setdict_object):
 
 def test_setdict___str__method(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     assert str(setdict_object) == "{set((1, 2, 3)): 'a'}"
 
 
 def test_setdict___repr__method(setdict_object):
     """docstring"""
-    setdict_object.set(set((1, 2, 3,)), "a")
-
     assert repr(setdict_object) == "'{set((1, 2, 3)): 'a'}'"
